41c1a385387ecb9cbbefaba3cb246e88d9aa7a0b
shalom-nganga <minnomacharia@gmail.com>
2023-11-09 11:44:25 +0300

    completed assignments

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -5,7 +5,22 @@ function Tickler() {
     console.log("Teehee!");
   }
 
-  return <button>Tickle me!</button>;
+  // return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
 
 export default Tickler;
+
+
+
+// function Clickable({ onClick }) {
+//   return <button onClick={onClick}>Click Me</button>;
+// }
+
+// function App() {
+//   function handleClick() {
+//     console.log("click");
+//   }
+
+//   return <Clickable onClick={handleClick} />;
+// }
